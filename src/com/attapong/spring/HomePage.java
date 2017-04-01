0e35b2872704699427d852784d6cf1f2e056f149
Attpong maneepan <Attpong maneepan>
2017-04-01 17:12:06 +0700

    first commit

---
+++ src/com/attapong/spring/HomePage.java
@@ -0,0 +1,15 @@
+package com.attapong.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePage {
+
+	
+	@RequestMapping("/")
+	public String showHomePage(){
+		
+		return "main-menu";
+	}
+}
